b55011d8381fe4ee0f49789f279b34a0718954e8
Theng <wai.loon.theng@accenture.com>
2020-05-27 07:27:03 +0800

    Added hello world endpoint

--- src/main/kotlin/com/example/springbootkotlinhelloworld/SpringBootKotlinHelloWorldApplication.kt
+++ src/main/kotlin/com/example/springbootkotlinhelloworld/SpringBootKotlinHelloWorldApplication.kt
@@ -2,10 +2,22 @@ package com.example.springbootkotlinhelloworld
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.net.InetAddress
 
 @SpringBootApplication
 class SpringBootKotlinHelloWorldApplication
-
 fun main(args: Array<String>) {
 	runApplication<SpringBootKotlinHelloWorldApplication>(*args)
+}
+
+@RestController
+class MyController {
+
+	@GetMapping("/")
+	fun helloWorld(): String {
+		return "Hello, World! from ${InetAddress.getLoopbackAddress().hostAddress}"
+	}
+
 }
